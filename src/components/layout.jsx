d2c5cca1a66c07b72fd80e5ee57477c583bc84c8
Lokii0007 <lokeshyadav1098@gmail.com>
2024-03-01 16:18:49 +0530

    responsive

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import "../css/layout.css"
 
 export default function Layout() {
+
+    const [bgUrl , setBgUrl] = useState("bg1")
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth <= 600) {
+            setBgUrl("bg3")
+          } else {
+            setBgUrl("bg1")
+          }
+        }
+    
+        handleResize()
+        window.addEventListener("resize", handleResize)
+    
+        return () => {
+          window.removeEventListener("resize", handleResize)
+        }
+      }, [])
+
     return (
         <>
-            <div style={{ backgroundImage: "url(bg-1.png)" }} className="layout position-fixed">
+            <div style={{ backgroundImage: `url(${bgUrl}.svg)` }} className="layout position-fixed">
                 <div className="layout-top text-center">
                     FOTF
                 </div>
