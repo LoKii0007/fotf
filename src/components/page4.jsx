771687979ce127d684e86aea5a2cb06d0ba5e095
Lokii0007 <lokeshyadav1098@gmail.com>
2024-03-10 04:54:40 +0530

    pag4-added

--- src/components/page4.jsx
+++ src/components/page4.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../css/page4.css"
 import { gsap } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Page4({scale}) {
+
+    const [flag , setFlag] = useState(false)
 
     // --------------naagin animation------------------ 
 
@@ -147,6 +149,38 @@ export default function Page4({scale}) {
 
     }, []);
 
+        // --------------------setting state of flag--------------------- 
+
+        useEffect(()=>{
+
+            // const resetAnimations = () => {
+            //     const topImg = document.querySelector(".fotf-top-img");
+            //     const bottomImg = document.querySelector(".fotf-bottom-img");
+                
+            //     topImg.style.animation = "none";
+            //     bottomImg.style.animation = "none";
+
+            //     void topImg.offsetWidth;
+            //     void bottomImg.offsetWidth;
+            //     topImg.style.animation = "slide-top 8s linear infinite";
+            //     bottomImg.style.animation = "slide-bottom 8s linear infinite";
+            // }
+
+           ScrollTrigger.create({
+            trigger:".fotf",
+            start:"bottom bottom",
+            end:"bottom 200%",
+            onEnter:()=>{
+                setFlag(true)
+            },
+            onEnterBack:()=>{
+                // resetAnimations()
+                setFlag(false)
+            }
+           })
+        },[])
+
+
     return (
         <>
             <div className="page-4 d-flex flex-column">
@@ -172,40 +206,71 @@ export default function Page4({scale}) {
                     <div className='char-2 char position-absolute '>O</div>
                     <div className='char-3 char position-absolute '>T</div>
                     <div className='char-4 char position-absolute '>F</div>
-                    {/* <div className="fotf-bg position-absolute d-flex flex-column">
-                        <div className="images">
+                    <div className="fotf-bg position-absolute d-flex flex-column">
+                        <div className="fotf-images d-flex flex-column justify-content-evenly align-items-center">
+                            <div style={{animation:flag?"slide-top 8s linear infinite":"" }} className="fotf-top-img d-flex align-items-start">
+                                <img className='fotf-img' src="/ALIENS0152.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1753.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0080.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0153.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1754.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0081.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0154.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1755.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0082.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0155.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1756.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0083.png" alt="" />
+                            </div>
+                            <div style={{animation:flag?"slide-bottom 8s linear infinite":"" }} className="fotf-bottom-img d-flex align-items-end">
+                                <img className='fotf-img' src="/ALIENS0156.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1757.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0093.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0157.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1758.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0094.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0158.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1759.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0095.png" alt="" />
+                                <img className='fotf-img' src="/ALIENS0159.png" alt="" />
+                                <img className='fotf-img' src="/ELFOS1766.png" alt="" />
+                                <img className='fotf-img' src="/HUMANOS0096.png" alt="" />
+                            </div>
                         </div>
-                    </div> */}
+                    </div>
                 </div>
 
-                {/* <div className="footer p-4 d-flex justify-content-between align-items-center">
+                <div className="footer px-5 d-flex justify-content-between align-items-center">
                     <div className="ft-left d-flex">
-                        <div className="privacy">
+                        <div className="privacy d-flex">
                             <div className="f-sq"></div>
-                            <Link className="privacy-text f-text" to="/privacy">Privacy Policy</Link>
+                            {/* <Link className="privacy-text f-text" to="/privacy">Privacy Policy</Link> */}
                             <div className='f-text'>Privacy Policy</div>
                         </div>
-                        <div className="terms px-3">
+                        <div className="term d-flex px-5">
                             <div className="f-sq"></div>
-                            <Link className="terms-text f-text" to="/terms" >Terms of use</Link>
+                            {/* <Link className="terms-text f-text" to="/terms" >Terms of use</Link> */}
                             <div className='f-text'>Terms of use</div>
                         </div>
-                        <div className="legal">
+                        <div className="legal d-flex">
                             <div className="f-sq"></div>
-                            <Link className="legal-text f-text" to="/legal" >Legal liscence</Link>
+                            {/* <Link className="legal-text f-text" to="/legal" >Legal liscence</Link> */}
                             <div className='f-text'>Legal liscence</div>
                         </div>
                     </div>
 
                     <div className="ft-right d-flex justify-content-center align-items-center">
+                        <div className="d-logo pe-2 f-logo">
+                            <i className="fa-brands fa-discord"></i>
+                        </div>
                         <div className="d-logo f-logo">
-                            <i class="fa-brands fa-discord"></i>
+                            <i className="fa-brands fa-discord"></i>
                         </div>
                         <div className="t-logo f-logo">
-                            <i class="fa-brands fa-x-twitter"></i>
+                            <i className="fa-brands fa-x-twitter"></i>
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </>
     )
