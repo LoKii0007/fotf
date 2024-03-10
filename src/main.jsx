c6f7cea4b024c38bd07846be6656f8d0e120db38
Lokii0007 <lokeshyadav1098@gmail.com>
2024-03-10 15:14:54 +0530

    speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {inject} from "@vercel/analytics"
 import './index.css'
+import {inject} from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 inject()
+injectSpeedInsights()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
